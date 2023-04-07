eede6b1f1c006bc1ca3e13da5426ee934ea4e685
Asma Bouhlel boughdira <abouhlel@c1r4p2.42nice.fr>
2023-04-07 13:16:07 +0200

    run dquote> nest generate module auth dquote> nest generate controller auth dquote> nest generate service auth dquote> npm install --save-dev @nestjs/testing dquote> npm install axios dquote> npm install --save-dev @types/jest dquote> npm install --save-dev @types/mocha

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { ChatModule } from './chat/chat.module';
 import { UsersModule } from './users/users.module';
 import { RoomModule } from './room/room.module';
 import { RoomLstModule } from './room_lst/room_lst.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ChatModule, UsersModule, RoomModule, RoomLstModule],
+  imports: [ChatModule, UsersModule, RoomModule, RoomLstModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
