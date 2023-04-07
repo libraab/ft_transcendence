e7690dfdc64e3a08039de54e613e5ac9b484f439
Asma Bouhlel boughdira <abouhlel@c1r4p2.42nice.fr>
2023-04-07 15:14:47 +0200

    fixed error404, changed path in controler

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Query, Get} from '@nestjs/common';
 import axios from 'axios';
 
 @Controller('auth')
 export class AuthController {
-  @Post('token')
-  async getAccessToken(@Body() body: { code: string }): Promise<{ access_token: string }> {
-    const { code } = body;
-    const response = await axios.post('https://example.com/oauth2/token', {
-      grant_type: 'authorization_code',
-      code,
-      redirect_uri: 'http://localhost:3000/auth/callback',
-      client_id: 'CLIENT_ID',
-      client_secret: 'CLIENT_SECRET',
-    });
+  @Get()
+  async getAccessToken(@Query() query: any[]): Promise<string> {
+    // const { code } = query;
+    // const response = await axios.post('https://example.com/oauth2/token', {
+    //   grant_type: 'authorization_code',
+    //   code,
+    //   redirect_uri: 'http://localhost:3000/auth/callback',
+    //   client_id: 'CLIENT_ID',
+    //   client_secret: 'CLIENT_SECRET',
+    // });
 
-    return { access_token: response.data.access_token };
+    return ("Hello Asma!");
   }
 }
