52b7d64dd4ce545acd87e2435e96ebb5f8af2539
6L45 <silas.dbr170@gmail.com>
2023-05-02 10:50:08 +0200

    cleanup useless directories rmrf

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
-import { UsersModule } from './users/users.module';
 import { RoomLstModule } from './room_lst/room_lst.module';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
@@ -11,7 +10,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatModule, UsersModule, RoomLstModule, AuthModule, DashboardModule, DatabaseModule, PrismaModule, ConfigModule.forRoot({
+  imports: [ChatModule, RoomLstModule, AuthModule, DashboardModule, DatabaseModule, PrismaModule, ConfigModule.forRoot({
     isGlobal: true,
   }),],
   controllers: [AppController],
