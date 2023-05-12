7dc65f8fee708642923b67fea01b60f93bb26173
libraab <81954460+libraab@users.noreply.github.com>
2023-05-13 00:57:41 +0200

    sign in with 42

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,97 +1,126 @@
 <script>
-	import Header from "./component/Header.svelte";
-	import Footer from "./component/Footer.svelte";
+    import {Route, Router} from "svelte-navigator";
+    import Login from "./component/login/Login.svelte";
+    import Header from "./component/Header.svelte";
+    import Footer from "./component/Footer.svelte";
 	import Tabs from "./shared/Tabs.svelte";
 	import Dashboard from "./component/dashboard/Dashboard.svelte";
-    import { loop_guard } from "svelte/internal";
-    import Chat from "./component/chat/Chat.svelte";
-    import FriendList from "./shared/FriendList.svelte";
+	import Chat from "./component/chat/Chat.svelte";
+	import PrivateRoute from "./PrivateRoute.svelte";
+	import { userId42 } from "./stores";
 
-	let tabs = ['Dashboard', 'Game', 'Chat', 'Rooms'];
-	let activeTab = 'Dashboard';
-	
-	let id42 = 4444;
-	let data = {};
+    let tabs = ['Dashboard', 'Game', 'Chat', 'Rooms'];
+    let activeTab = 'Dashboard';
 
-	let img_path = "img/il_794xN.3892173164_egqv.avif";
+    let data = {};
 
-	const switchTab = (e) => {
-		activeTab = e.detail;
-	}
-	
-	async function fetchData()
-	{
-		try {
-			const response = await fetch(`http://localhost:3000/dashboard/${id42}`);
-			data = await response.json();
-		}
-		catch (error) {
-			console.error(error);
-		}
-		
-		return data;
-	}
+    let img_path = "img/il_794xN.3892173164_egqv.avif";
 
-	const handleNewImgPath = (event) => {
-		console.log(event.detail);
-		console.log("img gonna be upload");
-	}
+    const switchTab = (e) => {
+        activeTab = e.detail;
+    }
+
+    async function fetchData() {
+        try {
+            const cookieValue = document.cookie
+                .split('; ')
+                .find(cookie => cookie.startsWith('jwt_cookie'))
+                .split('=')[1];
+
+            const id42 = document.cookie
+                .split('; ')
+                .find(cookie => cookie.startsWith('id42'))
+                .split('=')[1];
+			$userId42 = id42;
+            console.log(cookieValue);
+            const response = await fetch(`http://localhost:3000/dashboard/${id42}`, {
+                headers: {
+                    'Authorization': `Bearer ${cookieValue}`
+                }
+            });
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                throw new Error('Failed to fetch dashboard data');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+        return data;
+    }
+
+    const handleNewImgPath = (event) => {
+        console.log(event.detail);
+        console.log("img gonna be upload");
+    }
 </script>
 
+<Router>
 
-<Header {img_path} />
+    <Header {img_path}/>
 
-{#await fetchData()}
-<p>Loading...</p>
+    <main>
+        <Route path="login">
+            <Login/>
+        </Route>
+        <PrivateRoute path="/">
+            	{#await fetchData()}
+            	<p>Loading...</p>
 
-{:then dashboardData}
-{#if dashboardData}
+            	{:then dashboardData}
+            	{#if dashboardData}
 
-<Tabs {activeTab} {tabs} id={dashboardData.id} on:tabChange={ switchTab } />
-<main>
-		<div class="main_body">
-			{#if activeTab === "Dashboard"}
-				<Dashboard data={dashboardData} on:updateImg={ handleNewImgPath }/>
+            	<Tabs {activeTab} {tabs} id={dashboardData.id} on:tabChange={ switchTab } />
+                    			<div class="main_body">
+                    				{#if activeTab === "Dashboard"}
+                    					<Dashboard data={dashboardData} on:updateImg={ handleNewImgPath }/>
 
-			{:else if activeTab === "Game"}
-					<p>Here to play bro</p>
+                    				{:else if activeTab === "Game"}
+                    						<p>Here to play bro</p>
 
-			{:else if activeTab === "Chat"}
-					<Chat/>
+                    				{:else if activeTab === "Chat"}
+                    						<Chat/>
 
-			{:else if activeTab === "Rooms"}
-					<p>talk to everyone</p>
+                    				{:else if activeTab === "Rooms"}
+                    						<p>talk to everyone</p>
 
-			{/if}
-		</div>
-</main>
-	{:else}
-		<h1>┌∩┐(◕_◕)┌∩┐</h1>
-		<p>sry but nop</p>
-	{/if}
+                    				{/if}
+                    			</div>
+            		{:else}
+            			<h1>┌∩┐(◕_◕)┌∩┐</h1>
+            			<p>sry but nop</p>
+            		{/if}
 
-			{:catch error}
-				<h3>Error: {error.message}</h3>
+            				{:catch error}
+            					<h3>Error: {error.message}</h3>
 
-	{/await}
+            		{/await}
+        </PrivateRoute>
 
-<Footer/>
 
+    </main>
+
+
+    <Footer/>
+</Router>
 
 <style>
-	main{
-		background: #ececec;
-		padding: 20px 0px;;
-		width: 100%;
-	}
-	.main_body {
-		/* height: 50vh; 33% de la hauteur de la fenêtre */
-		width: 100%; /* 100% de la largeur de la fenêtre */
-		/* background: url('path/to/img.png') center/cover no-repeat, blue; */
+    main {
+        background: #ececec;
+        padding: 20px 0px;;
+        width: 100%;
+    }
 
-		/* color: white; */
-		margin: 0 auto;
-		font-size: 6px;
-		font-size: 1vw;
-	}
+    .main_body {
+        /* height: 50vh; 33% de la hauteur de la fenêtre */
+        width: 100%; /* 100% de la largeur de la fenêtre */
+        /* background: url('path/to/img.png') center/cover no-repeat, blue; */
+
+        /* color: white; */
+        margin: 0 auto;
+        font-size: 6px;
+        font-size: 1vw;
+    }
 </style>
