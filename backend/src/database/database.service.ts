804a42ab1dd50017eec174be75ac32b66f071725
6L45 <silas.dbr170@gmail.com>
2023-05-24 20:13:32 +0200

    room details

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -478,6 +478,57 @@ export class DatabaseService
 
 		return roomIdsAndNames;
 	}
+
+	async getRoomDetailsByClientId(clientId: number):
+	Promise<{ roomId: number; roomName: string; client2: Clients | null; owner: Clients | null; secu: number | null }[]> {
+		const roomMembers = await this.prisma.roomMembers.findMany({
+			where: {
+				memberId: clientId,
+			},
+			select: {
+				room: {
+					select: {
+						id: true,
+						name: true,
+						client2: {
+							select: {
+								id: true,
+								name: true,
+								id42: true,
+								img: true,
+								cookie: true,
+								Dfa: true,
+								DfaSecret: true
+							},
+						},
+						owner: {
+							select: {
+								id: true,
+								name: true,
+								id42: true,
+								img: true,
+								cookie: true,
+								Dfa: true,
+								DfaSecret: true
+							},
+						},
+						secu: true,
+					},
+				},
+			},
+		});
+
+		const roomDetails = roomMembers.map((roomMember) => ({
+			roomId: roomMember.room.id,
+			roomName: roomMember.room.name,
+			client2: roomMember.room.client2 ?? null,
+			owner: roomMember.room.owner ?? null,
+			secu: roomMember.room.secu ?? null,
+		}));
+
+		return roomDetails;
+	}
+
 /*
 	async getRoomIdsAndNamesByClientId(clientId: number): Promise<{ roomId: number; roomName: string }[]> {
 		const roomWithOwner = await this.prisma.rooms.findFirst({
