4e825f26000d6652c0177a87f76f7d2c2581a4cf
6L45 <silas.dbr170@gmail.com>
2023-06-24 22:44:48 +0200

    svelte erase

--- frontend/src/hostname.js
+++
@@ -1,2 +0,0 @@
-const hostname = "localhost";
-export { hostname };
--- frontend/src/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-const id42 = document.cookie ? document.cookie
-    .split('; ')
-    .find(cookie => cookie.startsWith('id42'))
-    .split('=')[1] : null;
-
-export const userId42 = writable(id42);
-export let page_shown = writable("/")
